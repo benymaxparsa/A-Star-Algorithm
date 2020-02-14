2b52fbf5967a846786016b5e0360bef585a33345
Parsa KamaliPour <parsakamalipour@gmail.com>
2020-02-15 02:23:56 +0330

    day 0

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public int size;
+    public Node start;
+    public Node end;
+
+    private bool[,] walls;
+
+    List<Node> visited;
+    List<Node> option;
+
+    Node[,] matrix;
+
+    private void Start()
+    {
+        matrix = new Node[size, size];
+        walls = new bool[size, size];
+        walls[size/2, size/2] = true;
+    }
+
+    public void Created() 
+    {
+
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public float GCost;  // distance from start Node
+    public float HCost;  // distance from end Node
+    public float FCost;  // H + G cost
+
+    public Vector2Int pos;
+
+    public Node parent;   //ref
+
+    public bool isWall = false;
+
+    public void Calc_GCost()
+    {
+        GCost = parent.GCost + Vector2Int.Distance(pos, parent.pos) ; 
+    }
+
+    public void Calc_HCost(Vector2Int final)                        //........................change to grid end...........................//
+    {
+        GCost =  Vector2Int.Distance(pos, final);
+    }
+
+    public void Calc_FCost()
+    {
+        FCost = GCost + HCost;
+    }
+}
